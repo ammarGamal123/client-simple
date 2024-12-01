8ce4e021cac7e8ff572bc55ae41a3e8b45e17540
ammarGamal123 <amargamal832909@gmail.com>
2024-12-01 17:40:30 +0200

    Implement client connections to the server and enable message broadcasting to all connected clients.clients so that it can connects with the server and broadcasts message with every client connected to the client

--- client.js
+++ client.js
@@ -4,5 +4,16 @@ const WebSocket = require('ws');
 // specified ip for address is live in my local machine
 const serverAddress = "ws://127.0.0.1:5000";
 
+// this opens connection with the server but no thing happens
 const ws = new WebSocket(serverAddress);
 
+
+// here send a message to a server
+ws.on('open',function(){
+    ws.send("Hello server!");
+});
+
+
+ws.on('message' , function(msg){
+    console.log("A message was received from server: " + msg);
+});
